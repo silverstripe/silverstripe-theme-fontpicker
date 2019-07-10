737cafcd5865d6befa78d10b2229507221d2ccc2
bergice <andre.kiste@silverstripe.com>
2019-07-10 17:18:47 +1200

    First commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+# Fontpicker Module
+
+[![Build Status](https://travis-ci.org/silverstripe/fontpicker.svg?branch=master)](https://travis-ci.org/silverstripe/fontpicker)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/silverstripe/fontpicker/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/silverstripe/fontpicker/?branch=master)
+[![codecov](https://codecov.io/gh/silverstripe/fontpicker/branch/master/graph/badge.svg)](https://codecov.io/gh/silverstripe/fontpicker)
+[![SilverStripe supported module](https://img.shields.io/badge/silverstripe-supported-0071C4.svg)](https://www.silverstripe.org/software/addons/silverstripe-commercially-supported-module-list/)
+
+This module adds a font picker under the admin settings, under a `Theme Options` tab. It comes with 3 fonts out of the box, provided by Google Fonts, but more fonts can be added.
+
+## Usage
+
+### Adding extra fonts
+
+To add extra fonts, simply set the `theme_fonts` field under `SiteConfig` as follows:
+
+```yaml
+SilverStripe\SiteConfig\SiteConfig:
+  theme_fonts:
+    metropolis: Metropolis
+    nunito-sans: Nunito Sans
+    fira-sans: Fira Sans
+    merriweather: Merriweather
+```
+
+> Note that any font which is unavailable at Google Fonts will not be displayed in preview in the font selector.
+ 
+### Changing the default font
+
+To set the default font, simply set the `default_font` field under `SiteConfig` as follows:
+
+```yaml
+SilverStripe\Fontpicker\Forms\FontPickerField:
+  default_font: metropolis
+```
+
+## Versioning
+
+This library follows [Semver](http://semver.org). According to Semver, you will be able to upgrade to any minor or patch version of this library without any breaking changes to the public API. Semver also requires that we clearly define the public API for this library.
+
+All methods, with `public` visibility, are part of the public API. All other methods are not part of the public API. Where possible, we'll try to keep `protected` methods backwards-compatible in minor/patch versions, but if you're overriding methods then please test your work before upgrading.
---
+++ src/Extensions/FontpickerSiteConfigExtension.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace SilverStripe\Fontpicker\Extensions;
+
+use SilverStripe\Fontpicker\Forms\FontPickerField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\View\Requirements;
+
+class FontpickerSiteConfigExtension extends DataExtension
+{
+    private static $db = array(
+        'MainFontFamily' => 'Varchar(50)',
+    );
+
+    /**
+     * Defines the theme fonts that can be selected via the CMS
+     *
+     * @config
+     * @var array
+     */
+    protected static $theme_fonts = [
+        'nunito-sans' => 'Nunito Sans',
+        'fira-sans' => 'Fira Sans',
+        'merriweather' => 'Merriweather',
+    ];
+
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $this->addFontPicker($fields);
+    }
+
+    protected function addFontPicker(FieldList $fields)
+    {
+        $fonts = $this->owner->config()->get('theme_fonts');
+
+        // Import each font via the google fonts api to render font preview
+        foreach ($fonts as $fontTitle) {
+            $fontFamilyName = str_replace(' ', '+', $fontTitle);
+            Requirements::css("//fonts.googleapis.com/css?family=$fontFamilyName");
+        }
+
+        $fields->addFieldsToTab(
+            'Root.ThemeOptions',
+            [
+                FontPickerField::create(
+                    'MainFontFamily',
+                    _t(
+                        __CLASS__ . '.MainFontFamily',
+                        'Main font family'
+                    ),
+                    $fonts
+                )
+            ]
+        );
+    }
+
+    /**
+     * If HeaderBackground is not set, assume no theme colours exist and populate some defaults if the colour
+     * picker is enabled. We don't use populateDefaults() because we don't want SiteConfig to re-populate its own
+     * defaults.
+     */
+    public function onBeforeWrite()
+    {
+        if (!$this->owner->MainFontFamily) {
+            $this->owner->update([
+                'MainFontFamily' => FontPickerField::getDefaultFont(),
+            ]);
+        }
+    }
+}
